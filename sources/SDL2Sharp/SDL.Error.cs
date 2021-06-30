92cc1e227062c55558dbb55609410aeda96d1579
Ronald van Manen <rvanmanen@gmail.com>
2021-06-30 14:20:47 +0200

    Added samples project with C# implementation of Code Example from https://wiki.libsdl.org/SDL_Init:
    #include "SDL.h"
    
    int main(int argc, char* argv[])
    {
        if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
            SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
            return 1;
        }
    
        /* ... */
    
        SDL_Quit();
    
        return 0;
    }

---
+++ sources/SDL2Sharp/SDL.Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace SDL2Sharp
+{
+    public static unsafe partial class SDL
+    {
+        public static string GetErrorString()
+        {
+            return new string(GetError());
+        }
+    }
+}
---
+++ sources/SDL2Sharp/SDL_LogPriority.cs
@@ -0,0 +1,13 @@
+namespace SDL2Sharp
+{
+    public enum SDL_LogPriority
+    {
+        SDL_LOG_PRIORITY_VERBOSE = 1,
+        SDL_LOG_PRIORITY_DEBUG,
+        SDL_LOG_PRIORITY_INFO,
+        SDL_LOG_PRIORITY_WARN,
+        SDL_LOG_PRIORITY_ERROR,
+        SDL_LOG_PRIORITY_CRITICAL,
+        SDL_NUM_LOG_PRIORITIES,
+    }
+}
