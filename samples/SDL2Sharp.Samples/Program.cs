4e5199de9c955a261b1bffe32b4a287e384a11a6
Ronald van Manen <rvanmanen@gmail.com>
2021-06-30 14:44:20 +0200

    Allow native library to be loaded from a user-specified directory.

--- samples/SDL2Sharp.Samples/Program.cs
+++ samples/SDL2Sharp.Samples/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace SDL2Sharp.Samples
+﻿using System;
+
+namespace SDL2Sharp.Samples
 {
     internal static class Program
     {
         private static int Main(string[] args)
         {
+            SDL.LibraryDirectory = (Environment.Is64BitProcess)
+                ? @"..\..\..\..\packages\sdl2-2.0.14-win32-x64"
+                : @"..\..\..\..\packages\sdl2-2.0.14-win32-x86";
+
             if (SDL.Init(SDL.SDL_INIT_VIDEO | SDL.SDL_INIT_AUDIO) != 0)
             {
                 SDL.Log($"Unable to initialize SDL: {SDL.GetErrorString()}");
