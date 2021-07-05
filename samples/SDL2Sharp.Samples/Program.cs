75e5e71676a9f459cc2254a653af768cc9b16497
Ronald van Manen <rvanmanen@gmail.com>
2021-07-05 14:39:25 +0200

    Expanded sample with the creation (and timed destruction) of a window.

--- samples/SDL2Sharp.Samples/Program.cs
+++ samples/SDL2Sharp.Samples/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Threading;
 
 namespace SDL2Sharp.Samples
 {
-    internal static class Program
+    internal static unsafe class Program
     {
         private static int Main(string[] args)
         {
@@ -16,8 +17,32 @@ namespace SDL2Sharp.Samples
                 return 1;
             }
 
-            SDL.Log("Hello World!");
+            // Create an application window with the following settings:
+            var window = SDL.CreateWindow(
+                null,                                       // window title
+                (int)SDL.SDL_WINDOWPOS_UNDEFINED,           // initial x position
+                (int)SDL.SDL_WINDOWPOS_UNDEFINED,           // initial y position
+                640,                                        // width, in pixels
+                480,                                        // height, in pixels
+                (uint)SDL_WindowFlags.SDL_WINDOW_OPENGL     // flags - see below
+            );
 
+            // Check that the window was successfully created
+            if (window == null)
+            {
+                // In the case that the window could not be made...
+                Console.WriteLine("Could not create window: %s\n", SDL.GetErrorString());
+                return 1;
+            }
+
+            // The window is open: could enter program loop here (see SDL_PollEvent())
+
+            Thread.Sleep(3000);  // Pause execution for 3000 milliseconds, for example
+
+            // Close and destroy the window
+            SDL.DestroyWindow(window);
+
+            // Clean up
             SDL.Quit();
 
             return 0;
