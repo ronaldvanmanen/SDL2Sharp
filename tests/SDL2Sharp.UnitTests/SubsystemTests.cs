44e3a6beda177d1796a6c68e1238c8b4b532753c
Ronald van Manen <rvanmanen@gmail.com>
2021-07-25 14:13:33 +0200

    Skip unit tests requiring SDL2.dll because the required runtime package is not downloaded when running unit tests from CI.

--- tests/SDL2Sharp.UnitTests/SubsystemTests.cs
+++ tests/SDL2Sharp.UnitTests/SubsystemTests.cs
@@ -4,7 +4,7 @@ namespace SDL2Sharp.UnitTests
 {
     public static class SubsystemTests
     {
-        [Fact]
+        [Fact(Skip = "Skipped because the runtime packages are not downloaded when in CI.")]
         public static void TestConstructor()
         {
             using (new Subsystem(0)) 
