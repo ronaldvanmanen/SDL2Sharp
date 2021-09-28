1b7e7a3ca880c9a13b722cb1b9f58d238587ae4e
Ronald van Manen <rvanmanen@gmail.com>
2021-09-28 12:44:46 +0200

    Forgot to update P/Invoke generation scripts after updating libsdl2 and libsdl2ttf packages.

--- generation/sdl2/generate.ps1
+++ generation/sdl2/generate.ps1
@@ -1,1 +1,1 @@
-& dotnet tool run ClangSharpPInvokeGenerator "@settings.rsp" --file-directory "$HOME\.nuget\packages\libsdl2\2.0.16\lib\native\include"
+& dotnet tool run ClangSharpPInvokeGenerator "@settings.rsp" --file-directory "$HOME\.nuget\packages\libsdl2\2.0.16-1261613060\lib\native\include"
