9ba513740a92e0cf1136598b932ca49780bd19d2
Ronald van Manen <rvanmanen@gmail.com>
2022-09-22 12:22:43 +0200

    Updated scripts for generating interop code for SDL2 and SDL2TTF libraries.
    Use the version of the NuGet packages referenced by SDL2Sharp.Interop instead of using a fixed version which needs to be updated each time we update one of the package references.

--- generation/sdl2/generate.ps1
+++ generation/sdl2/generate.ps1
@@ -1,1 +1,11 @@
-& dotnet tool run ClangSharpPInvokeGenerator "@settings.rsp" --file-directory "$HOME\.nuget\packages\libsdl2\2.0.18-1629963310\lib\native\include"
+$allPackages = (dotnet list ..\..\sources\SDL2Sharp.Interop\SDL2Sharp.Interop.csproj package)
+$libsdl2Package = $allPackages | Select-String -Pattern '^ +> libsdl2 +'
+$libsdl2VersionFound = $libsdl2Package -match '^ +> libsdl2 +(?<requestedVersion>[^ ]+) +(?<resolvedVersion>[^ ]+)'
+if (-not $libsdl2VersionFound)
+{
+  throw "libsdl2 version not found"
+}
+
+$libsdl2Version = $Matches.resolvedVersion
+
+& dotnet tool run ClangSharpPInvokeGenerator "@settings.rsp" --file-directory "$HOME\.nuget\packages\libsdl2\$libsdl2Version\lib\native\include"
--- generation/sdl2ttf/generate.ps1
+++ generation/sdl2ttf/generate.ps1
@@ -1,1 +1,25 @@
-& dotnet tool run ClangSharpPInvokeGenerator "@settings.rsp" --file-directory "$HOME\.nuget\packages\libsdl2ttf\2.0.15-1629963746\lib\native\include" --include-directory "$HOME\.nuget\packages\libsdl2\2.0.18-1629963310\lib\native\include"
+$allPackages = (dotnet list ..\..\sources\SDL2Sharp.Interop\SDL2Sharp.Interop.csproj package)
+
+$libsdl2Package = $allPackages | Select-String -Pattern '^ +> libsdl2 +'
+$libsdl2VersionFound = $libsdl2Package -match '^ +> libsdl2 +(?<requestedVersion>[^ ]+) +(?<resolvedVersion>[^ ]+)'
+if ($libsdl2VersionFound)
+{
+  $libsdl2Version = $Matches.resolvedVersion
+}
+else
+{
+  throw "libsdl2 version not found"
+}
+
+$libsdl2ttfPackage = $allPackages | Select-String -Pattern '^ +> libsdl2ttf +'
+$libsdl2ttfVersionFound = $libsdl2ttfPackage -match '^ +> libsdl2ttf +(?<requestedVersion>[^ ]+) +(?<resolvedVersion>[^ ]+)'
+if ($libsdl2ttfVersionFound)
+{
+  $libsdl2ttfVersion = $Matches.resolvedVersion
+}
+else
+{
+  throw "libsdl2ttf version not found"
+}
+
+& dotnet tool run ClangSharpPInvokeGenerator "@settings.rsp" --file-directory "$HOME\.nuget\packages\libsdl2ttf\$libsdl2ttfVersion\lib\native\include" --include-directory "$HOME\.nuget\packages\libsdl2\$libsdl2Version\lib\native\include"
