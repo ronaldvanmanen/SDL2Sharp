9231039364161489c9e77332f6f13ffa26035271
Ronald van Manen <rvanmanen@gmail.com>
2022-10-02 20:44:33 +0200

    NuGetize SDL2_ttf libraries based on latest release added to SDL_ttf repo on GitHub (https://github.com/libsdl-org/SDL_ttf).

--- scripts/nugetize-sdl2ttf.ps1
+++ scripts/nugetize-sdl2ttf.ps1
@@ -4,6 +4,13 @@ function Create-Directory([string[]] $Path) {
   }
 }
 
+function Copy-File([string[]] $Path, [string] $Destination, [switch] $Force) {
+  if (!(Test-Path -Path $Destination)) {
+    New-Item -Path $Destination -Force:$Force -ItemType "Directory" | Out-Null
+  }
+  Copy-Item -Path $Path -Destination $Destination -Force:$Force
+}
+
 try {
   $RepoRoot = Join-Path -Path $PSScriptRoot -ChildPath ".."
 
@@ -21,13 +28,11 @@ try {
   $DownloadsDir = Join-Path -Path $RepoRoot -ChildPath "downloads"
   Create-Directory -Path $DownloadsDir
 
-  $Releases = Invoke-WebRequest https://www.libsdl.org/projects/SDL_ttf/release/ -UseBasicParsing
-
-  $Hrefs = $Releases.Links.href |? { $_ -like "SDL2_ttf-devel-*-VC.zip" }
-  $LatestHref = ($Hrefs | Sort-Object -Descending {[System.Version]($_ | Select-String '((?:\d{1,3}\.){2}\d{1,3})').Matches[0].Groups[1].Value})[0]
-  $LatestVersion = ($LatestHref | Select-String '((?:\d{1,3}\.){2}\d{1,3})').Matches[0].Groups[1].Value
-  $ZipDownloadPath = Join-Path $DownloadsDir $LatestHref
-  $DownloadPath = Join-Path -Path $DownloadsDir -ChildPath "SDL2_ttf-$LatestVersion"
+  $LatestRelease = Invoke-RestMethod -Headers @{ 'Accept'='application/vnd.github+json'} -Uri https://api.github.com/repos/libsdl-org/SDL_ttf/releases/latest
+  $LatestVersion = $LatestRelease.name
+  $LatestAsset = $LatestRelease.assets |? { $_.name -Like "SDL2_ttf-devel-*-VC.zip" }
+  $LatestAssetName = $LatestAsset.name
+  $BrowserDownloadUrl = $LatestAsset.browser_download_url
 
   $PackageVersion = $LatestVersion
 
@@ -37,39 +42,68 @@ try {
     }
   }
 
-  if (!(Test-Path $DownloadPath))
-  {
-    if (!(Test-Path $ZipDownloadPath)) {
-      Write-Host "Downloading SDL2_TTF development libraries version $LatestVersion to $ZipDownloadPath..." -ForegroundColor Yellow
-      (New-Object System.Net.WebClient).DownloadFile("https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-$LatestVersion-VC.zip", $ZipDownloadPath)
-    }
+  $ZipDownloadPath = Join-Path $DownloadsDir $LatestAssetName
 
-    Write-Host "Extracting SDL2_TTF development libraries version $LatestVersion..." -ForegroundColor Yellow
-    Expand-Archive -Path $ZipDownloadPath -DestinationPath $DownloadsDir -Force
+  if (!(Test-Path $ZipDownloadPath)) {
+    Write-Host "Downloading SDL2_ttf development libraries version '$LatestVersion' from '$BrowserDownloadUrl'..." -ForegroundColor Yellow
+    Invoke-WebRequest -Uri $BrowserDownloadUrl -OutFile $ZipDownloadPath
   }
 
+  Write-Host "Extracting SDL2_ttf development libraries to '$DownloadsDir'..." -ForegroundColor Yellow
+  $ExpandedFiles = Expand-Archive -Path $ZipDownloadPath -DestinationPath $DownloadsDir -Force -Verbose *>&1
+
+  Write-Host "Staging SDL2_ttf development libraries to '$StagingDir'..." -ForegroundColor Yellow
   Copy-Item -Path $PackagesDir\libsdl2ttf -Destination $StagingDir -Force -Recurse
   Copy-Item -Path $PackagesDir\libsdl2ttf.runtime.win-x64 -Destination $StagingDir -Force -Recurse
   Copy-Item -Path $PackagesDir\libsdl2ttf.runtime.win-x86 -Destination $StagingDir -Force -Recurse
-  Copy-Item -Path "$DownloadPath\include" -Destination "$StagingDir\libsdl2ttf" -Force -Recurse
-  Copy-Item -Path "$DownloadPath\lib\x64\SDL2_ttf.dll" -Destination "$StagingDir\libsdl2ttf.runtime.win-x64" -Force
-  Copy-Item -Path "$DownloadPath\lib\x86\SDL2_ttf.dll" -Destination "$StagingDir\libsdl2ttf.runtime.win-x86" -Force
 
+  $ExpandedFiles | Foreach-Object {
+    if ($_.message -match "Created '(.*)'.*") {
+      $ExpandedFile = $Matches[1]
+        
+      if (($ExpandedFile -like '*\CHANGES.txt') -or
+          ($ExpandedFile -like '*\LICENSE.txt') -or
+          ($ExpandedFile -like '*\README.txt')) {
+        Copy-File -Path $ExpandedFile -Destination $StagingDir\libsdl2ttf -Force
+        Copy-File -Path $ExpandedFile -Destination $StagingDir\libsdl2ttf.runtime.win-x64 -Force
+        Copy-File -Path $ExpandedFile -Destination $StagingDir\libsdl2ttf.runtime.win-x86 -Force
+      }
+      elseif ($ExpandedFile -like '*\docs\*.md') {
+        Copy-File -Path $ExpandedFile -Destination $StagingDir\libsdl2ttf\docs -Force
+        Copy-File -Path $ExpandedFile -Destination $StagingDir\libsdl2ttf.runtime.win-x64\docs -Force
+        Copy-File -Path $ExpandedFile -Destination $StagingDir\libsdl2ttf.runtime.win-x86\docs -Force
+      }
+      elseif ($ExpandedFile -like '*\include\*.h') {
+        Copy-File -Path $ExpandedFile -Destination $StagingDir\libsdl2ttf\lib\native\include -Force
+      }
+      elseif ($ExpandedFile -like '*\lib\x64\*.dll') {
+        Copy-File -Path $ExpandedFile -Destination $StagingDir\libsdl2ttf.runtime.win-x64\runtimes\win-x64\native -Force
+      }
+      elseif ($ExpandedFile -like '*\lib\x86\*.dll') {
+        Copy-File -Path $ExpandedFile -Destination $StagingDir\libsdl2ttf.runtime.win-x86\runtimes\win-x86\native -Force
+      }
+    }
+  }
+
+  Write-Host "Replace variable `$version`$ in runtime.json with value '$PackageVersion'..." -ForegroundColor Yellow
   $RuntimeContent = Get-Content "$StagingDir\libsdl2ttf\runtime.json" -Raw
   $RuntimeContent = $RuntimeContent.replace('$version$', $PackageVersion)
-  Set-Content "$StagingDir\libsdl2ttf\runtime.json" $RuntimeContent
+  Set-Content $StagingDir\libsdl2ttf\runtime.json $RuntimeContent
 
-  & nuget pack "$StagingDir\libsdl2ttf\libsdl2ttf.nuspec" -Properties version=$PackageVersion -OutputDirectory "$ArtifactsPkgDir"
+  Write-Host "Build 'libsdl2ttf' package..." -ForegroundColor Yellow
+  & nuget pack $StagingDir\libsdl2ttf\libsdl2ttf.nuspec -Properties version=$PackageVersion -OutputDirectory $ArtifactsPkgDir
   if ($LastExitCode -ne 0) {
     throw "'nuget pack' failed for 'libsdl2ttf.nuspec'"
   }
 
-  & nuget pack "$StagingDir\libsdl2ttf.runtime.win-x64\libsdl2ttf.runtime.win-x64.nuspec" -Properties version=$PackageVersion -OutputDirectory "$ArtifactsPkgDir"
+  Write-Host "Build 'libsdl2ttf.runtime.win-x64' package..." -ForegroundColor Yellow
+  & nuget pack $StagingDir\libsdl2ttf.runtime.win-x64\libsdl2ttf.runtime.win-x64.nuspec -Properties version=$PackageVersion -OutputDirectory $ArtifactsPkgDir
   if ($LastExitCode -ne 0) {
     throw "'nuget pack' failed for 'libsdl2ttf.runtime.win-x64.nuspec'"
   }
 
-  & nuget pack "$StagingDir\libsdl2ttf.runtime.win-x86\libsdl2ttf.runtime.win-x86.nuspec" -Properties version=$PackageVersion -OutputDirectory "$ArtifactsPkgDir"
+  Write-Host "Build 'libsdl2ttf.runtime.win-x86' package..." -ForegroundColor Yellow
+  & nuget pack $StagingDir\libsdl2ttf.runtime.win-x86\libsdl2ttf.runtime.win-x86.nuspec -Properties version=$PackageVersion -OutputDirectory $ArtifactsPkgDir
   if ($LastExitCode -ne 0) {
     throw "'nuget pack' failed for 'libsdl2ttf.runtime.win-x86.nuspec'"
   }
