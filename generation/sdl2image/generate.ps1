7ea2f9452e1dd8eb6f4593b5c0ac49bf67bb72f7
Ronald van Manen <rvanmanen@gmail.com>
2022-11-14 12:45:57 +0100

    Use variable with package name and use that instead of repeating package name in strings (DRY).

--- generation/sdl2image/generate.ps1
+++ generation/sdl2image/generate.ps1
@@ -1,25 +1,31 @@
-$allPackages = (dotnet list ..\..\sources\SDL2Sharp.Interop\SDL2Sharp.Interop.csproj package)
+try {
+  $PackageList = (dotnet list ..\..\sources\SDL2Sharp.Interop\SDL2Sharp.Interop.csproj package)
 
-$libsdl2Package = $allPackages | Select-String -Pattern '^ +> libsdl2 +'
-$libsdl2VersionFound = $libsdl2Package -match '^ +> libsdl2 +(?<requestedVersion>[^ ]+) +(?<resolvedVersion>[^ ]+)'
-if ($libsdl2VersionFound)
-{
-  $libsdl2Version = $Matches.resolvedVersion
-}
-else
-{
-  throw "libsdl2 version not found"
-}
+  $SDL2PackageName = "SDL2"
+  $SDL2Package = $PackageList | Select-String -Pattern "^ +> $SDL2PackageName +"
+  $SDL2PackageVersionFound = $SDL2Package -match "^ +> $SDL2PackageName +(?<requestedVersion>[^ ]+) +(?<resolvedVersion>[^ ]+)"
+  if (-not $SDL2PackageVersionFound)
+  {
+    throw "$SDL2PackageName version not found"
+  }
 
-$libsdl2imagePackage = $allPackages | Select-String -Pattern '^ +> libsdl2image +'
-$libsdl2imageVersionFound = $libsdl2imagePackage -match '^ +> libsdl2image +(?<requestedVersion>[^ ]+) +(?<resolvedVersion>[^ ]+)'
-if ($libsdl2imageVersionFound)
-{
-  $libsdl2imageVersion = $Matches.resolvedVersion
-}
-else
-{
-  throw "libsdl2image version not found"
-}
+  $SDL2PackageVersion = $Matches.resolvedVersion
 
-& dotnet tool run ClangSharpPInvokeGenerator "@settings.rsp" --file-directory "$HOME\.nuget\packages\libsdl2image\$libsdl2imageVersion\lib\native\include" --include-directory "$HOME\.nuget\packages\libsdl2\$libsdl2Version\lib\native\include"
+  $SDL2ImagePackageName = 'libsdl2image'
+  $SDL2ImagePackage = $PackageList | Select-String -Pattern "^ +> $SDL2ImagePackageName +"
+  $SDL2ImagePackageVersionFound = $SDL2ImagePackage -match "^ +> $SDL2ImagePackageName +(?<requestedVersion>[^ ]+) +(?<resolvedVersion>[^ ]+)"
+  if (-not $SDL2ImagePackageVersionFound)
+  {
+    throw "$SDL2ImagePackageName version not found"
+  }
+
+  $SDL2ImagePackageVersion = $Matches.resolvedVersion
+
+  & dotnet tool run ClangSharpPInvokeGenerator "@settings.rsp" --file-directory "$HOME\.nuget\packages\$SDL2ImagePackageName\$SDL2ImagePackageVersion\lib\native\include" --include-directory "$HOME\.nuget\packages\$SDL2PackageName\$SDL2PackageVersion\lib\native\include"
+}
+catch {
+  Write-Host -Object $_
+  Write-Host -Object $_.Exception
+  Write-Host -Object $_.ScriptStackTrace
+  exit 1
+}
